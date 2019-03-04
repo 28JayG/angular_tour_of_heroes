4aaf95a886751d8fad168642d739f540ff01c37a
28JayG <jaygandhi.32@outlook.com>
2019-03-04 19:14:54 +0530

    *Initial routing done

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,16 +1,18 @@
 import 'package:angular/angular.dart';
-import 'package:angular_tour_of_heros/src/hero_list_component.dart';
 import 'package:angular_tour_of_heros/src/hero_services.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:angular_tour_of_heros/src/routes.dart';
 
 @Component(
-    selector: 'my-app',
-    template: '''
+  selector: 'my-app',
+  template: '''
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <router-outlet  [routes]="Routes.all"></router-outlet>
   ''',
-    directives: [HeroListComponent],
-    providers: [ClassProvider(HeroServices)])
+  directives: [routerDirectives],
+  providers: [ClassProvider(HeroServices)],
+  exports: [Routes, RoutePaths],
+)
 class AppComponent {
   final String title = 'Tour of Heroes';
 }
